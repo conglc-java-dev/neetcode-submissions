b7836111361a0e27eb2d6a6b915e96fbc41ddf38
Lê Chí Công <Lechicong9a2@gmail.com>
2026-07-31 17:32:09 +0700

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = 0;
+        int sell = 1;
+        int maxProfit = 0;
+        for (sell = 1; sell < prices.length; sell++) {
+         maxProfit= Math.max( prices[sell]-prices[buy],maxProfit) ;
+         if(prices[sell]<prices[buy]){
+            buy=sell;
+         }
+
+        }
+        return maxProfit;
+    }
+}
