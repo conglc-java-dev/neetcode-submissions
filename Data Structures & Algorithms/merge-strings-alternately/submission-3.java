580a952065f4918be74e573551100d872b6a20ae
Lê Chí Công <Lechicong9a2@gmail.com>
2026-09-05 16:31:45 +0700

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int w1 =0; 
+        int w2=0;
+        int maxlength = Math.max(word1.length(),word2.length());
+        StringBuilder res = new StringBuilder() ;
+        for(int i=0;i<maxlength;i++){
+            if(w1<word1.length()) res.append(word1.charAt(w1));
+            if(w2<word2.length()) res.append(word2.charAt(w2));
+            w1 ++;
+            w2 ++ ;
+        }  
+        return res.toString();
+    }
+}
